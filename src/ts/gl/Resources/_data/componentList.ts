ca8464286190f77d2541819d0d992dc4b14f1531
ukonpower <ore.ukonpower@gmail.com>
2024-10-09 00:42:43 +0900

    いらんもの消す

--- src/ts/gl/Resources/_data/componentList.ts
+++ src/ts/gl/Resources/_data/componentList.ts
@@ -32,22 +32,11 @@ import { Wave } from '../Components/Demo3/Stage/Wave/index.ts';
 import { WetGround } from '../Components/Demo3/Stage/WetGround/index.ts';
 import { SideTex } from '../Components/Demo3/Textures/SideTex/index.ts';
 import { TuringTex } from '../Components/Demo3/Textures/TuringTex/index.ts';
-import { DashCube } from '../Components/Entities/Effects/DashCube/index.ts';
-import { FlashLine } from '../Components/Entities/Effects/FlashLine/index.ts';
-import { GridCross } from '../Components/Entities/Effects/GridCross/index.ts';
-import { FluidCrystal } from '../Components/Entities/FluidCrystal/index.ts';
-import { MatchMove } from '../Components/Entities/MatchMove/index.ts';
-import { SkyBox } from '../Components/Entities/SkyBox/index.ts';
-import { TextEffect } from '../Components/Entities/TextEffect/index.ts';
-import { TestComponent } from '../Components/Test/TestComponent/index.ts';
 import { BLidgeClient } from '../Components/Utilities/BLidgeClient/index.ts';
 import { Text } from '../Components/Utilities/Text/index.ts';
-import { TurnTable } from '../Components/Utilities/TurnTable/index.ts';
 import { LookAt } from '../Components/View/LookAt/index.ts';
 import { OrbitControls } from '../Components/View/OrbitControls/index.ts';
-import { RotateViewer } from '../Components/View/RotateViewer/index.ts';
 import { ShakeViewer } from '../Components/View/ShakeViewer/index.ts';
-import { VJCamera } from '../Components/View/VJCamera/index.ts';
 
 export const COMPONENTLIST: {[key: string]: any} = {
 	Camera: {
@@ -88,30 +77,13 @@ export const COMPONENTLIST: {[key: string]: any} = {
 			TuringTex,
 		},
 	},
-	Entities: {
-		Effects: {
-			DashCube,
-			FlashLine,
-			GridCross,
-		},
-		FluidCrystal,
-		MatchMove,
-		SkyBox,
-		TextEffect,
-	},
-	Test: {
-		TestComponent,
-	},
 	Utilities: {
 		BLidgeClient,
 		Text,
-		TurnTable,
 	},
 	View: {
 		LookAt,
 		OrbitControls,
-		RotateViewer,
 		ShakeViewer,
-		VJCamera,
 	},
 };
